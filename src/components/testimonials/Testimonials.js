0e7fed549003ba12b04c7fe3b5cec672cb373597
shan5742 <asamshan456@gmail.com>
2022-02-27 10:36:14 +0000

    tweaking testimonials

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -8,7 +8,6 @@ export const Testimonials = () => {
   return (
     <Layout>
       <div className={s.container}>
-        <Signup />
         {td.map((t) => (
           <Testimonial author={t.author} quote={t.quote} title={t.title} />
         ))}
@@ -34,21 +33,27 @@ const Testimonial = ({ quote, author, title }) => {
 
 const td = [
   {
-    author: "Mike Brock",
+    author: "Mike",
     title: "Software Engineer",
     quote:
       "As someone learning web development from scratch I would not have been able to get to the position ready for a job in the industry without RDG's help. He has been patient and on-hand to help me through every stage of the process including HTML, CSS, Javascript, React, Github and more. He is incredibly knowledgeable in these areas and I cannot thank him enough for his support over the past 6 months.",
   },
   {
-    author: "Stephon Phillops",
+    author: "Stephon",
     title: "computer science grad",
     quote:
       "RDG has been super helpful, we focused more on the non-technical side of things but I think that is what I need moreso right now so I'm glad that was prioritized.",
   },
   {
-    author: "Memas Deligeorgakis",
+    author: "Memas",
     title: "Indie Hacker",
     quote:
       "The session was really helpful and valuable. I got to ask RDG questions that he had knowledge and experience about. He gave me valuable feedback and good ideas to think about! We are going to have frequent calls to exchange ideas and compare the feedback we are doing in our projects.",
+  },
+  {
+    author: "Dom",
+    title: "Aspiring Engineer",
+    quote:
+      "The world of programming is so overwhelming with all the possible paths, courses, articles, blogs and more. RDG has helped me focus on my own path and I am now making solid progress and ready to apply for jobs",
   },
 ];
